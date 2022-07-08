0a8f21efa277e2cfb2e2e14332e50ffba32632dc
Максим <maksgirya@mail.ru>
2022-07-08 12:23:02 +0300

    Priority and Category get/delete by ID controller methods

--- src/main/java/com/maksgir/tasklist/backend_springboot/controller/CategoryController.java
+++ src/main/java/com/maksgir/tasklist/backend_springboot/controller/CategoryController.java
@@ -2,13 +2,15 @@ package com.maksgir.tasklist.backend_springboot.controller;
 
 
 import com.maksgir.tasklist.backend_springboot.entity.Category;
+import com.maksgir.tasklist.backend_springboot.entity.Priority;
 import com.maksgir.tasklist.backend_springboot.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -21,4 +23,58 @@ public class CategoryController {
     public List<Category> showAllCategories() {
         return categoryRepository.findAll();
     }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("id must be empty", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Category> updateCategory(@RequestBody Category category) {
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        Optional<Category> optional = categoryRepository.findById(id);
+        Category category = null;
+        if (optional.isPresent()) {
+            category = optional.get();
+        } else {
+            return new ResponseEntity("Category with ID = " + id + " wasn't found", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(category);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategoryById(@PathVariable Long id) {
+        Optional<Category> optional = categoryRepository.findById(id);
+        Category category = null;
+        if (optional.isPresent()) {
+            categoryRepository.deleteById(id);
+            category = optional.get();
+        } else {
+            return new ResponseEntity("Category with ID = " + id + " wasn't found", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(category);
+    }
+
 }
--- src/main/java/com/maksgir/tasklist/backend_springboot/controller/PriorityController.java
+++ src/main/java/com/maksgir/tasklist/backend_springboot/controller/PriorityController.java
@@ -1,15 +1,17 @@
 package com.maksgir.tasklist.backend_springboot.controller;
 
 
+import com.maksgir.tasklist.backend_springboot.entity.Category;
 import com.maksgir.tasklist.backend_springboot.entity.Priority;
 import com.maksgir.tasklist.backend_springboot.repository.PriorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/priority")
@@ -21,5 +23,63 @@ public class PriorityController {
     @GetMapping()
     public List<Priority> showAllPriorities() {
         return priorityRepository.findAll();
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<Priority> addPriority(@RequestBody Priority priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("id must be empty", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> updatePriority(@RequestBody Priority priority) {
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Priority> getPriorityById(@PathVariable Long id) {
+        Optional<Priority> optional = priorityRepository.findById(id);
+        Priority priority = null;
+        if (optional.isPresent()) {
+            priority = optional.get();
+
+
+        } else {
+            return new ResponseEntity("Priority with ID = " + id + " wasn't found", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(priority);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Priority> deletePriorityById(@PathVariable Long id) {
+        Optional<Priority> optional = priorityRepository.findById(id);
+        Priority priority = null;
+        if (optional.isPresent()) {
+            priority = optional.get();
+            priorityRepository.deleteById(id);
+        } else {
+            return new ResponseEntity("Priority with ID = " + id + " wasn't found", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(priority);
     }
 }
